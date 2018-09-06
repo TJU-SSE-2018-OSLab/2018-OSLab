7cbaf350aa223f4755b16518ca4a59be8b460321
ZWZ <942740938@qq.com>
2018-09-06 15:08:23 +0800

    尝试解决多级目录问题

--- fs/misc.c
+++ fs/misc.c
@@ -111,27 +111,67 @@ PUBLIC int search_file(char * path)
 PUBLIC int strip_path(char * filename, const char * pathname,
 		      struct inode** ppinode)
 {
-	const char * s = pathname;
-	char * t = filename;
+    const char * s = pathname;
+    char * t = filename;
 
-	if (s == 0)
-		return -1;
+    if (s == 0)
+        return -1;
 
-	if (*s == '/')
-		s++;
+    if (*s == '/')
+        s++;
 
-	while (*s) {		/* check each character */
-		if (*s == '/')
-			return -1;
-		*t++ = *s++;
-		/* if filename is too long, just truncate it */
-		if (t - filename >= MAX_FILENAME_LEN)
-			break;
-	}
-	*t = 0;
+    struct inode *pinode_now = root_inode, *ptemp;
+    struct dir_entry * pde;
+    int dir_blk0_nr, nr_dir_blks, nr_dir_entries, m;
+    int i, j;
 
-	*ppinode = root_inode;
+    while(*s){
+        if(*s == '/'){
+            int flag = 0;
+            dir_blk0_nr = pinode_now->i_start_sect;
+            nr_dir_blks = (pinode_now->i_size + SECTOR_SIZE - 1) / SECTOR_SIZE;
+            nr_dir_entries =
+              pinode_now->i_size / DIR_ENTRY_SIZE; 
 
-	return 0;
+            m = 0;
+            pde = 0;
+            *t = 0;
+            //printl("filename:%s\n",filename);
+            for (i = 0; i < nr_dir_blks && flag==0; i++) {
+                RD_SECT(pinode_now->i_dev, dir_blk0_nr + i);
+                pde = (struct dir_entry *)fsbuf;
+                for (j = 0; j < SECTOR_SIZE / DIR_ENTRY_SIZE; j++,pde++) {
+                    if (strcmp(filename, pde->name) == 0){
+                        ptemp = get_inode(pinode_now->i_dev, pde->inode_nr);
+                        if(ptemp->i_mode == I_DIRECTORY){
+                            pinode_now = ptemp;
+                            flag = 1;
+                            break;
+                        }
+                    }
+                    if (++m > nr_dir_entries)
+                        return -1;
+                }
+                if (m > nr_dir_entries || flag==0) {
+                    return -1;
+                }
+            }
+            if(flag == 0){
+                return -1;
+            }
+            t = filename;
+
+            s++;
+        }
+        else{
+            *t++ = *s++;
+            if (t - filename >= MAX_FILENAME_LEN)
+                break;
+        }
+    }
+
+    *t = 0;
+    *ppinode = pinode_now;
+    return 0;
 }
 
