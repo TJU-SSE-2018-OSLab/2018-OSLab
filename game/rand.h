27372b2028f35970665f8b450256a2dba172ab3f
MrNickell <songtianyou07@gmail.com>
2018-09-06 14:12:43 +0800

    尝试添加2048

---
+++ game/rand.h
@@ -0,0 +1,26 @@
+/********************************************
+ * @file   rand.h
+ * @brief  Simple implementation of rand(reference: 
+ *     https://stackoverflow.com/questions/4768180/rand-implementation/)
+ * @author Jianfeng HOU
+ *******************************************/
+
+#ifndef _RAND_H_
+#define _RNAD_H_
+
+#include "type.h"
+
+#define RAND_MAX 32768
+
+static unsigned long next = 1;
+
+PUBLIC int rand2048(void) {
+    next = next * 1103515245 + 12345;
+    return (unsigned int) (next / 65536) % RAND_MAX;
+}
+
+PUBLIC void srand2048(unsigned int seed) {
+    next = seed;
+}
+
+#endif // _RAND_H_
